e392629def5eba0b7ea1142d40f4d5dd7dec924e
Breder <lucasbreder@gmail.com>
2025-11-06 06:49:42 -0300

    fix: tamanho de fonte dos cases

--- components/CaseInfo.tsx
+++ components/CaseInfo.tsx
@@ -10,10 +10,10 @@ export const CaseInfo = ({
 }) => {
   return (
     <div className={"mb-3"}>
-      <h3 className="border-b border-inherit pb-1 w-1/2 max-w-[200px] font-bold">
+      <h3 className="border-b border-inherit pb-1 w-1/2 max-w-[200px] font-bold  fhdv:portrait:text-2xl">
         {title}
       </h3>
-      <div className="mt-3 case-content">
+      <div className="mt-3 case-content fhdv:portrait:text-xl  fhdv:portrait:leading-8">
         <Markdown>{text}</Markdown>
       </div>
     </div>
