92043e4125dc5663135615cfa1f94f84c5b9730c
Breder <lucasbreder@gmail.com>
2025-11-06 12:39:10 -0300

    feat:cria estrutura para carregamento de imagens

--- data/images.ts
+++ data/images.ts
@@ -2,16 +2,8 @@ import { Images } from "@/types/Image";
 
 export const images: Images[] = [
   {
-    id: 1,
-    gallery: ["/images/01/1.pdf"],
-  },
-  {
     id: 2,
     gallery: ["/images/02/FECOOGAP.2.jpeg"],
-  },
-  {
-    id: 3,
-    gallery: ["/images/03/ACFROG~1.PDF"],
   },
   {
     id: 5,
@@ -20,10 +12,6 @@ export const images: Images[] = [
   {
     id: 8,
     gallery: ["/images/08/0J6A1284 (1).jpg", "/images/08/0J6A1284.jpg"],
-  },
-  {
-    id: 9,
-    gallery: ["/images/09/SITE-06.jpg.pdf"],
   },
   {
     id: 10,
@@ -55,19 +43,6 @@ export const images: Images[] = [
     ],
   },
   {
-    id: 15,
-    gallery: ["/images/15/Cooperativas_20Mirins-páginas-1.pdf"],
-  },
-  {
-    id: 16,
-    gallery: [
-      "/images/16/N_C3_ADvel_20II_20Pr_C3_A1ticas_20Nota_2010_Fazenda_20Sustent_C3_A1vel-1.pdf",
-      "/images/16/N_C3_ADvel_20II_20Pr_C3_A1ticas_20Nota_2010_Fazenda_20Sustent_C3_A1vel-21.pdf",
-      "/images/16/N_C3_ADvel_20II_20Pr_C3_A1ticas_20Nota_2010_Fazenda_20Sustent_C3_A1vel-2.pdf",
-      "/images/16/N_C3_ADvel_20II_20Pr_C3_A1ticas_20Nota_2010_Fazenda_20Sustent_C3_A1vel-3.pdf",
-    ],
-  },
-  {
     id: 17,
     gallery: ["/images/17/WhatsApp_20Image_202022-04-28_20at_2018.28.34.jpeg"],
   },
@@ -88,90 +63,16 @@ export const images: Images[] = [
     gallery: ["/images/22/IMG_6723.jpg"],
   },
   {
-    id: 23,
-    gallery: [
-      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-12.pdf",
-      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-13.pdf",
-      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-16.pdf",
-      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-17.pdf",
-      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-18.pdf",
-      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-25.pdf",
-      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-4.pdf",
-    ],
-  },
-  {
     id: 24,
-    gallery: [], // Pasta vazia
-  },
-  {
-    id: 29,
-    gallery: [
-      "/images/29/ANEXOS_20-_20IMAGENS_20_2B_20CONTRATO-1.pdf",
-      "/images/29/ANEXOS_20-_20IMAGENS_20_2B_20CONTRATO-2.pdf",
-      "/images/29/ANEXOS_20-_20IMAGENS_20_2B_20CONTRATO-3.pdf",
-      "/images/29/ANEXOS_20-_20IMAGENS_20_2B_20CONTRATO-4.pdf",
-      "/images/29/ANEXOS_20-_20IMAGENS_20_2B_20CONTRATO-5.pdf",
-    ],
+    gallery: [], // Pasta vazia, mantida
   },
   {
     id: 34,
     gallery: ["/images/34/image_(40).png"],
   },
   {
-    id: 35,
-    gallery: [
-      "/images/35/Evid_C3_AAncias_201-mesclado_20(1)-10.pdf",
-      "/images/35/Evid_C3_AAncias_201-mesclado_20(1)-14.pdf",
-      "/images/35/Evid_C3_AAncias_201-mesclado_20(1)-15.pdf",
-      "/images/35/Evid_C3_AAncias_201-mesclado_20(1)-16.pdf",
-      "/images/35/Evid_C3_AAncias_201-mesclado_20(1)-1.pdf",
-    ],
-  },
-  {
-    id: 36,
-    gallery: [
-      "/images/36/Eventos_20sobre_20sistemas_20de_20produ_C3_A7_C3_A3o_20de_20uva-1.pdf",
-      "/images/36/Eventos_20sobre_20sistemas_20de_20produ_C3_A7_C3_A3o_20de_20uva-2.pdf",
-      "/images/36/Eventos_20sobre_20sistemas_20de_20produ_C3_A7_C3_A3o_20de_20uva-3.pdf",
-    ],
-  },
-  {
-    id: 38,
-    gallery: [
-      "/images/38/FCC_20-_20COP30-1.pdf",
-      "/images/38/FCC_20-_20COP30-3.pdf",
-      "/images/38/FCC_20-_20COP30-7.pdf",
-    ],
-  },
-  {
-    id: 39,
-    gallery: ["/images/39/PAV_20COOPMETRO.pdf"],
-  },
-  {
-    id: 41,
-    gallery: ["/images/41/Relat_C3_B3rio_20CRERAL.pdf"],
-  },
-  {
     id: 45,
     gallery: ["/images/45/02-01.webp"],
-  },
-  {
-    id: 46,
-    gallery: ["/images/46/ED-98-VERSAO-DIGITAL-p_C3_A1ginas.pdf"],
-  },
-  {
-    id: 49,
-    gallery: [
-      "/images/49/Revista_20Lar_20mar_C3_A7o_20abril_20maio_20new_202023_20internet.pdf",
-    ],
-  },
-  {
-    id: 57,
-    gallery: ["/images/57/FOTOS_PDF.pdf"],
-  },
-  {
-    id: 60,
-    gallery: ["/images/60/Projeto_20Energia_20Solar_(1).pdf"],
   },
   {
     id: 61,
