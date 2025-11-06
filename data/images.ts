8dca077990214ec43199af1db58ee5ba8e9ca5c4
Breder <lucasbreder@gmail.com>
2025-11-06 12:38:51 -0300

    feat: adiciona imagens

--- data/images.ts
+++ data/images.ts
@@ -3,6 +3,190 @@ import { Images } from "@/types/Image";
 export const images: Images[] = [
   {
     id: 1,
-    gallery: ["/intro/intro-pic1.png"],
+    gallery: ["/images/01/1.pdf"],
+  },
+  {
+    id: 2,
+    gallery: ["/images/02/FECOOGAP.2.jpeg"],
+  },
+  {
+    id: 3,
+    gallery: ["/images/03/ACFROG~1.PDF"],
+  },
+  {
+    id: 5,
+    gallery: ["/images/05/5.jpeg"],
+  },
+  {
+    id: 8,
+    gallery: ["/images/08/0J6A1284 (1).jpg", "/images/08/0J6A1284.jpg"],
+  },
+  {
+    id: 9,
+    gallery: ["/images/09/SITE-06.jpg.pdf"],
+  },
+  {
+    id: 10,
+    gallery: [
+      "/images/10/Comitiva-COOPERNORTE-em-visita-a-cooperativas-americanas.webp",
+      "/images/10/qual-e-o-destaque-do-i-forum-regional-do-sudeste-paraense-em-relacao-a-sustentabilidade-agricola-e-ao-sistema-plantio-direto.webp",
+      "/images/10/WhatsApp-Image-2023-09-25-at-15.11.32-1068x712.webp",
+    ],
+  },
+  {
+    id: 11,
+    gallery: [
+      "/images/11/_MG_7712.jpg",
+      "/images/11/_MG_7740 (1).jpg",
+      "/images/11/_MG_7740.jpg",
+      "/images/11/_MG_7768.jpg",
+      "/images/11/_MG_7796.jpg",
+      "/images/11/_MG_7952.jpg",
+    ],
+  },
+  {
+    id: 13,
+    gallery: [
+      "/images/13/6eed33bd-d5f1-4575-b5e1-2b3c53f0d99a.jpg",
+      "/images/13/agro_coopernorte_agroindustria_16abr24_Divulgacao.jpg",
+      "/images/13/LEK00350.jpg",
+      "/images/13/WE406436.jpg",
+      "/images/13/WE406447.jpg",
+    ],
+  },
+  {
+    id: 15,
+    gallery: ["/images/15/Cooperativas_20Mirins-páginas-1.pdf"],
+  },
+  {
+    id: 16,
+    gallery: [
+      "/images/16/N_C3_ADvel_20II_20Pr_C3_A1ticas_20Nota_2010_Fazenda_20Sustent_C3_A1vel-1.pdf",
+      "/images/16/N_C3_ADvel_20II_20Pr_C3_A1ticas_20Nota_2010_Fazenda_20Sustent_C3_A1vel-21.pdf",
+      "/images/16/N_C3_ADvel_20II_20Pr_C3_A1ticas_20Nota_2010_Fazenda_20Sustent_C3_A1vel-2.pdf",
+      "/images/16/N_C3_ADvel_20II_20Pr_C3_A1ticas_20Nota_2010_Fazenda_20Sustent_C3_A1vel-3.pdf",
+    ],
+  },
+  {
+    id: 17,
+    gallery: ["/images/17/WhatsApp_20Image_202022-04-28_20at_2018.28.34.jpeg"],
+  },
+  {
+    id: 19,
+    gallery: ["/images/19/adesivo_20carro.jpeg"],
+  },
+  {
+    id: 20,
+    gallery: ["/images/20/imagem_20(9).png"],
+  },
+  {
+    id: 21,
+    gallery: ["/images/21/IMG_20220716_124312.jpg"],
+  },
+  {
+    id: 22,
+    gallery: ["/images/22/IMG_6723.jpg"],
+  },
+  {
+    id: 23,
+    gallery: [
+      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-12.pdf",
+      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-13.pdf",
+      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-16.pdf",
+      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-17.pdf",
+      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-18.pdf",
+      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-25.pdf",
+      "/images/23/Documentos_Comprobat_C3_B3rio_Projeto_RECA_compressed_20(1)_(1) (2)-4.pdf",
+    ],
+  },
+  {
+    id: 24,
+    gallery: [], // Pasta vazia
+  },
+  {
+    id: 29,
+    gallery: [
+      "/images/29/ANEXOS_20-_20IMAGENS_20_2B_20CONTRATO-1.pdf",
+      "/images/29/ANEXOS_20-_20IMAGENS_20_2B_20CONTRATO-2.pdf",
+      "/images/29/ANEXOS_20-_20IMAGENS_20_2B_20CONTRATO-3.pdf",
+      "/images/29/ANEXOS_20-_20IMAGENS_20_2B_20CONTRATO-4.pdf",
+      "/images/29/ANEXOS_20-_20IMAGENS_20_2B_20CONTRATO-5.pdf",
+    ],
+  },
+  {
+    id: 34,
+    gallery: ["/images/34/image_(40).png"],
+  },
+  {
+    id: 35,
+    gallery: [
+      "/images/35/Evid_C3_AAncias_201-mesclado_20(1)-10.pdf",
+      "/images/35/Evid_C3_AAncias_201-mesclado_20(1)-14.pdf",
+      "/images/35/Evid_C3_AAncias_201-mesclado_20(1)-15.pdf",
+      "/images/35/Evid_C3_AAncias_201-mesclado_20(1)-16.pdf",
+      "/images/35/Evid_C3_AAncias_201-mesclado_20(1)-1.pdf",
+    ],
+  },
+  {
+    id: 36,
+    gallery: [
+      "/images/36/Eventos_20sobre_20sistemas_20de_20produ_C3_A7_C3_A3o_20de_20uva-1.pdf",
+      "/images/36/Eventos_20sobre_20sistemas_20de_20produ_C3_A7_C3_A3o_20de_20uva-2.pdf",
+      "/images/36/Eventos_20sobre_20sistemas_20de_20produ_C3_A7_C3_A3o_20de_20uva-3.pdf",
+    ],
+  },
+  {
+    id: 38,
+    gallery: [
+      "/images/38/FCC_20-_20COP30-1.pdf",
+      "/images/38/FCC_20-_20COP30-3.pdf",
+      "/images/38/FCC_20-_20COP30-7.pdf",
+    ],
+  },
+  {
+    id: 39,
+    gallery: ["/images/39/PAV_20COOPMETRO.pdf"],
+  },
+  {
+    id: 41,
+    gallery: ["/images/41/Relat_C3_B3rio_20CRERAL.pdf"],
+  },
+  {
+    id: 45,
+    gallery: ["/images/45/02-01.webp"],
+  },
+  {
+    id: 46,
+    gallery: ["/images/46/ED-98-VERSAO-DIGITAL-p_C3_A1ginas.pdf"],
+  },
+  {
+    id: 49,
+    gallery: [
+      "/images/49/Revista_20Lar_20mar_C3_A7o_20abril_20maio_20new_202023_20internet.pdf",
+    ],
+  },
+  {
+    id: 57,
+    gallery: ["/images/57/FOTOS_PDF.pdf"],
+  },
+  {
+    id: 60,
+    gallery: ["/images/60/Projeto_20Energia_20Solar_(1).pdf"],
+  },
+  {
+    id: 61,
+    gallery: ["/images/61/image_(45).png", "/images/61/image_(46).png"],
+  },
+  {
+    id: 64,
+    gallery: ["/images/64/image_(47).png", "/images/64/image_(48).png"],
+  },
+  {
+    id: 65,
+    gallery: ["/images/65/image_(49).png", "/images/65/image_(50).png"],
+  },
+  {
+    id: 66,
+    gallery: ["/images/66/31_1.png"],
   },
 ];
